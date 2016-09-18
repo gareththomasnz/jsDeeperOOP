5e8f37a65a335d709157637c023d330242345b54
gareththomasnz <gareththomasnz@yahoo.com>
2016-09-18 14:53:27 +1200

    third commit

--- script.js
+++ script.js
@@ -75,5 +75,64 @@
 
 //display(Object.getOwnPropertyDescriptor(cat, 'name'));
 
-})();
 
+//var arr = ['red', 'blue', 'green'];
+//
+//Object.defineProperty(Array.prototype, 'last', {get: function(){
+//        return this[this.length-1];
+//        }});
+//var last = arr.last;
+//var arr2 = ['one', 'two', 'three'];
+//display(arr.last);
+//display(arr2.last);
+
+//function Animal(voice){
+//    this.voice = voice || 'grunt';
+//}
+//Animal.prototype.speak = function(){
+//    display(this.voice);
+//};
+//
+//function Cat(name, color){
+//    Animal.call(this, 'Meow');
+//        this.name = name;
+//        this.color = color;
+//}
+
+class Animal{
+    constructor(voice){
+        this.voice = voice || 'grunt';
+        
+    }
+
+speak(){
+    display(this.voice);
+    }
+}
+
+class Cat extends Animal{
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+    }
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+var fluffy = new Cat('fluffy', 'white');
+var muffin = new Cat('muffin', 'Brown');
+
+fluffy.speak();
+
+//display(Cat.prototype);
+//display(muffin.age);
+//display(Cat.prototype === muffin._proto_.age);
+//display(fluffy.age);
+//display(Cat.prototype === fluffy._proto_.age);
+
+
+display(fluffy.age);
+display(muffin.age);
+
+})();
